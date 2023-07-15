84c3911c092ac7524873b33e3d848ea5b29b66d1
jualat <junis.alattar@outlook.de>
2023-07-15 20:33:01 +0200

    lowered test tolerance to 1e-4

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,16 +1,16 @@
 context("Testing calculate function")
 test_that("calculate correctly calculates BMI", {
-  expect_equal(calculate(70, 1.75), 22.85714, tolerance = 1e-5)
+  expect_equal(calculate(70, 1.75), 22.85714, tolerance = 1e-4)
 })
 
 context("Testing convertLbs function")
 test_that("convertLbs correctly converts lbs to kg", {
-  expect_equal(convertLbs(154.32), 70, tolerance = 1e-5)
+  expect_equal(convertLbs(154.32), 70, tolerance = 1e-4)
 })
 
 context("Testing convertInch function")
 test_that("convertInch correctly converts inches to meters", {
-  expect_equal(convertInch(68.11), 1.73, tolerance = 1e-5)
+  expect_equal(convertInch(68.11), 1.73, tolerance = 1e-4)
 })
 
 context("Testing interpret function")
